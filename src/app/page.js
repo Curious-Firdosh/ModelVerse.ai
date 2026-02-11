cafd067b7699d35a45a79a1e7fd9dd174e4eeec4
Curious-Firdosh <thefirdosh@gmail.com>
2026-02-11 13:36:56 +0530

    Project Setup Installing

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button>Helllo</Button>
+    </div>
+  )
+}
+
+export default page
